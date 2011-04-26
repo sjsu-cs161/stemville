f9c17197b8c4ebf72d80128f3186699e84f029fc
Steve Terpe <srterpe@gmail.com>
2011-04-26 09:08:33 -0700

    all country regiondata iso

--- rsrc/json/readme.md
+++ rsrc/json/readme.md
@@ -9,4 +9,19 @@ selectoptions.json : look at or ask me...
 graphs.json : arr[ISO][i]["path"]  => path to the location of the i-th graph of country ISO
 	      arr[ISO][i]["file"] => short path/file information for the user menus for the i-th graph of country ISO
 
-models.json  see graphs.json  
+models.json  see graphs.json
+
+full_ISO_3166.json: 	//NOTE this file is 1.2 MB.  If you only need ISO->country long name data, perhaps ISO-3166-1 (5Kb) is better for your needs.
+			
+			arr[ISO][0] => "country (level 0) long name"
+			
+			arr[ISO][1][i]["code"] => 'five to 7 letter region code for the ith level 1 region in this country]'
+			arr[ISO][1][i]["name"] => 'long name of i-th level 1 region'
+			
+			arr[ISO][2][i]["code"] => 'extended ISO for ith level 2 region'
+			arr[ISO][2][i]["name"] => 'long name of ith level 2 region'
+
+			arr[ISO][c][0]["lat"] => "latitude for this ISO country (level 0) center"
+			arr[ISO][c][0]["long"] => "longitude for center "" at level 0" 
+
+			//NOTE only level 0 center lat/longs are implemented
---
+++ rsrc/json/utils/ISOcrawler.php
@@ -0,0 +1,111 @@
+<?php
+
+$ISOS = array();
+$file_list = array();
+$dir_path = "org/eclipse/stem/data/geography";
+if ($dhandle = opendir($dir_path))
+{
+	$i = 0;
+	while(false !== ($file = readdir($dhandle)))
+	{
+		if ($file == '.' || $file == '..') continue;
+		if (! is_dir($file)) if (strlen($file) == 20)
+		{
+			$file_list[$i++] = $file;
+		}
+	}
+	closedir($dhandle);
+}
+//asort($file_list);
+//echo var_dump($file_list);
+foreach($file_list as $key =>$file)
+{
+	if ($fhandle = fopen($dir_path."/".$file, "r"))
+	{
+		$level = 0;
+		$l0 = false;
+		//echo "opened file : ".$file."\n";
+		while(($buffer = fgets($fhandle, 4096)) !== false)
+		{
+			$buffer = utf8_encode($buffer); //unlike most of eclipses files these are not encoded properly thanks IBM
+			if($buffer{0} === "#")
+				continue;
+			
+			$exploded = explode("=", $buffer."=");
+			if (trim($exploded[0]) ==="") continue;
+			if (strlen(trim($exploded[0])) > 6) $level = 2;
+			if ($level == 0) 
+			{
+				$l0 = true;
+				$l1 = false;
+				$l2 = false;
+				$cc = trim($exploded[0]);
+				$ISOS[$cc] = array();
+				$ISOS[$cc][0] = trim($exploded[1]);
+				$ISOS[$cc][1] = array();
+				$ISOS[$cc][2] = array();
+				$ISOS[$cc]['c'] = ""; 
+				$level = 1;
+				$c = 0;
+				$d = 0;
+				continue;
+			}
+			if ($level == 1)
+			{
+				$l1 = true;
+				$ISOS[$cc][$level][$c] = array();
+				$ISOS[$cc][$level][$c]["code"] = trim($exploded[0]);
+				$ISOS[$cc][$level][$c++]["name"] = trim($exploded[1]);
+				continue;
+			}
+			if ($level == 2)
+			{
+				$l2 = true;
+				$ISOS[$cc][$level][$d] = array();
+				$ISOS[$cc][$level][$d]["code"] = trim($exploded[0]);
+				$ISOS[$cc][$level][$d++]["name"] = trim($exploded[1]);
+				continue;
+			}
+
+		}
+		if (!$l1)
+		{
+			$ISOS[$cc][1][$c] = array();
+			$ISOS[$cc][1][$c]["code"] = "N/A";
+			$ISOS[$cc][1][$c]["name"] = "No Level 1 Data Available";
+		}
+		if (!$l2)
+		{
+			$ISOS[$cc][2][$d] = array();
+			$ISOS[$cc][2][$d]["code"] = "N/A";
+			$ISOS[$cc][2][$d]["name"] = "No Level 2 Data Available";
+		}
+		fclose($fhandle);
+	}
+	if ($fhandle = fopen($dir_path."/centers/".$cc."_centers.properties", "r"))
+	{
+		$level = 0;
+		while(($buffer = fgets($fhandle, 4096)) !== false)
+		{
+			$buffer = utf8_encode($buffer);
+			if ($buffer{0} === "#")
+				continue;
+			$exploded = explode("=", $buffer."=");
+			if (trim($exploded[0])==="") continue;
+			if ($level == 0)
+			{
+				$ISOS[$cc]['c'] = array();
+				$ISOS[$cc]['c'][0] = array();
+				$temp = trim($exploded[1]);
+				$LL = explode(",", $temp.",");
+				$ISOS[$cc]['c'][0]["lat"] = trim($LL[0]);
+				$ISOS[$cc]['c'][0]["long"] = trim($LL[1]);
+				$level = 1 ;
+			}//THIS ONLY PARSES OUT NATIONAL CENTER LAT_LONGS FOR NOW
+		}
+	}
+}
+asort($ISOS);
+//echo var_dump($ISOS);
+echo json_encode($ISOS);
+?>
