36d905fb751966c81612385d6f123ed7c8b7f29b
Steve Terpe <srterpe@gmail.com>
2011-04-26 17:38:09 -0700

    fixed probs fullISO.json, some cntry have lvl3 info

--- rsrc/json/utils/ISOcrawler.php
+++ rsrc/json/utils/ISOcrawler.php
@@ -16,71 +16,61 @@ if ($dhandle = opendir($dir_path))
 	}
 	closedir($dhandle);
 }
-//asort($file_list);
-//echo var_dump($file_list);
+
 foreach($file_list as $key =>$file)
 {
 	if ($fhandle = fopen($dir_path."/".$file, "r"))
 	{
-		$level = 0;
 		$l0 = false;
-		$l1 = false;
-		$l2 = false;
-		//echo "opened file : ".$file."\n";
 		while(($buffer = fgets($fhandle, 4096)) !== false)
 		{
-			$buffer = utf8_encode($buffer); //unlike most of eclipses files these are not encoded properly thanks IBM
+			$buffer = utf8_encode($buffer); //these are not encoded properly thanks IBM
 			if($buffer{0} === "#")
 				continue;
 			$exploded = explode("=", $buffer."=");
-			if (trim($exploded[0]) ==="") continue;
-			if (strlen(trim($exploded[0])) > 6) $level = 2;
+			if (($lftside = trim($exploded[0])) ==="") continue;
+			$level = substr_count($lftside, "-");//you also made up ISO-codes that don't conform to length standards so I hope this hypen counting hack works...thanks
 			if ($level == 0) 
 			{
-				//$l0 = true;
-				//$l1 = false;
-				//$l2 = false;
-				$cc = trim($exploded[0]);
+				if ($l0) continue;//necessary b/c MKD file is completely wrong and everything gets overwritten as level 0 thanks ibm
+				$l0 = true;
+				$cc = $lftside;
 				$ISOS[$cc] = array();
 				$ISOS[$cc][0] = trim($exploded[1]);
 				$ISOS[$cc][1] = array();
 				$ISOS[$cc][2] = array();
+				$ISOS[$cc][3] = array();
 				$ISOS[$cc]['c'] = ""; 
 				$level = 1;
 				$c = 0;
 				$d = 0;
+				$e = 0;
 				continue;
 			}
 			if ($level == 1)
 			{
-				$l1 = true;
 				$ISOS[$cc][$level][$c] = array();
-				$ISOS[$cc][$level][$c]["code"] = trim($exploded[0]);
+				$ISOS[$cc][$level][$c]["code"] = $lftside;
 				$ISOS[$cc][$level][$c++]["name"] = trim($exploded[1]);
 				continue;
 			}
 			if ($level == 2)
 			{
-				$l2 = true;
 				$ISOS[$cc][$level][$d] = array();
-				$ISOS[$cc][$level][$d]["code"] = trim($exploded[0]);
+				$ISOS[$cc][$level][$d]["code"] = $lftside;
 				$ISOS[$cc][$level][$d++]["name"] = trim($exploded[1]);
 				continue;
 			}
+			if ($level == 3)
+			{
+				$ISOS[$cc][$level][$e] = array();
+				$ISOS[$cc][$level][$e] ["code"] = $lftside;
+				$ISOS[$cc][$level][$e++]["name"] = trim($exploded[1]);
+				
+			}
 
 		}
-		if (!$l1)
-		{
-			$ISOS[$cc][1][$c] = array();
-			//$ISOS[$cc][1][$c]["code"] = "N/A";
-			//$ISOS[$cc][1][$c]["name"] = "No Level 1 Data Available";
-		}
-		if (!$l2)
-		{
-			$ISOS[$cc][2][$d] = array();
-			//$ISOS[$cc][2][$d]["code"] = "N/A";
-			//$ISOS[$cc][2][$d]["name"] = "No Level 2 Data Available";
-		}
+
 		fclose($fhandle);
 	}
 	if ($fhandle = fopen($dir_path."/centers/".$cc."_centers.properties", "r"))
