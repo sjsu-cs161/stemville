da8d5ed7464b2ce034509ab6a1ba43d85b170ed4
Daniel Shin <dys12586@gmail.com>
2011-05-13 01:25:59 -0700

    updated backend path settings

--- backend/settings.php
+++ backend/settings.php
@@ -1,10 +1,15 @@
 <?php
 
-$ROOT_PATH = '/cs161/group2/';
+if (!defined('STEMVILLE_ROOT_PATH'))
+    define('STEMVILLE_ROOT_PATH', dirname(dirname(__FILE__)) . '/');
 
-define('STEMVILLE_ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . $ROOT_PATH . '/stemville/');
-define('STEM_ROOT_PATH', $_SERVER['DOCUMENT_ROOT'] . $ROOT_PATH . '/stem/');
+if (!defined('BASE_PATH'))
+    define('BASE_PATH', dirname(STEMVILLE_ROOT_PATH));
 
+if (!defined('STEM_ROOT_PATH'))
+    define('STEM_ROOT_PATH', BASE_PATH . '/stem/');
+
+#$STEM_COMMAND = 'cd ' . STEM_ROOT_PATH . ' && export DISPLAY=:0 && nohup '.$this->command.' > ' . ROOT_PATH . '/stemweblog/ 2>&1 & echo $!';
 $STEM_COMMAND = 'cd /home/tseng/group2/stem && export DISPLAY=:0 && nohup '.$this->command.' > /home/tseng/group2/stemweblog 2>&1 & echo $!';
 
 ?>
