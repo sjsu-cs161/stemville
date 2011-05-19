d9439811dafa12fad8be7b633531e14c4d87f999
Mikael Bjorkstam <bjorkstam@gmail.com>
2011-05-19 16:28:28 -0700

    comments + fixes

--- backend/process.php
+++ backend/process.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'settings.php';
+// SCRIPT FOUND ON  PHP.NET WEBSITE AND SLIGHTLY MODIFIED
 
 /* An easy way to keep in track of external processes.
  * Ever wanted to execute a process in php, but you still wanted to have somewhat controll of the process ? Well.. This is a way of doing it.
@@ -16,37 +17,35 @@ class Process{
             $this->runCom();
         }
     }
+    // Executes command and stores PID when called
     private function runCom(){
 	
 	$STEM_COMMAND = 'cd ' . STEM_ROOT_PATH . ' && export DISPLAY=:0 && nohup '.$this->command.' > ' . BASE_PATH . '/stemweblog 2>&1 & echo $!';
-        $command = $STEM_COMMAND;
-//	echo $command. '<br/>';
-        //exec("export DISPLAY=:0");
-	//exec("cd /home/tseng/group2/stem");
-        exec($command ,$op);
+        exec($STEM_COMMAND ,$op);
         $this->pid = (int)$op[0];
     }
 
+    // Set PID of Process instance
     public function setPid($pid){
         $this->pid = $pid;
     }
-
+    // PID getter
     public function getPid(){
         return $this->pid;
     }
-
+    // Get status -- running or not?
     public function status(){
         $command = 'ps -p '.$this->pid;        
         exec($command,$op);
         if (!isset($op[1]))return false;
         else return true;
     }
-
+    // Execute command (start process)
     public function start(){
         if ($this->command != '')$this->runCom();
         else return true;
     }
-
+    // Stop a process
     public function stop(){
         $command = 'kill '.$this->pid;
         exec($command);
